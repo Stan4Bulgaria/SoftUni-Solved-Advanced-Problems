0fd057a2009d6e086cfd57a8314bc77dbb88be4a
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:51:00 +0300

    Add files via upload

---
+++ Strong Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Strong_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int temNum = num;
+            int factorialSum = 0;
+            while (temNum > 0)
+            {
+                int currentNum = temNum % 10;
+                temNum /= 10;
+                int curFactorial = 1;
+                for (int i = 1; i <= currentNum; i++)
+                {
+                    curFactorial *= i;
+                }
+                factorialSum += curFactorial;
+
+            }
+            string result = factorialSum == num ? "yes" : "no";
+            Console.WriteLine(result);
+
+        }
+    }
+}
