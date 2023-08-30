ff95af78e5681f927dd8699203d6408f0dcbb04d
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:56:43 +0300

    Add files via upload

---
+++ Tuple/MyTuple.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class MyTuple<T,T2,T3>
+        
+    {
+        public T Item1 { get; set; }
+        public T2 Item2 { get; set; }
+        public T3 Item3 { get; set; }
+        public MyTuple(T item1, T2 item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }   
+        public MyTuple(T item1,T2 item2,T3 item3) :this(item1, item2)
+        {
+            Item3 = item3;
+        }
+        
+        public override string ToString()
+           =>$"{Item1} -> {Item2} -> {Item3}";
+        
+
+
+    }
+}
---
+++ Tuple/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split();
+            string infoOfPerson = string.Join(" ", input[0], input[1]);
+            string cityInfo = input[3];
+            if (input.Length == 5)
+            {
+                cityInfo = string.Join(" ", input[3], input[4]);
+            }
+            
+            MyTuple<string, string, string> personInfo = new MyTuple<string, string, string>
+                (infoOfPerson, input[2],  cityInfo);
+
+            input = Console.ReadLine().Split();
+            bool drunkOrNot = false;
+            if (input[2] == "drunk")
+            {
+                drunkOrNot = true;
+            }
+            MyTuple<string, int, bool> markInfo = new MyTuple<string, int, bool>
+                (input[0], int.Parse(input[1]), drunkOrNot);
+
+            input = Console.ReadLine().Split();
+            MyTuple<string, double, string> doubleInfo = new MyTuple<string, double, string>
+                (input[0], double.Parse(input[1]), input[2]);
+
+            Console.WriteLine(personInfo.ToString());
+            Console.WriteLine(markInfo.ToString());
+            Console.WriteLine(doubleInfo.ToString());
+
+        }
+    }
+}
