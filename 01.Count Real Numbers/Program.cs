73a25c00eefb48b6069888281c1d483990e39634
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:07:53 +0300

    Add files via upload

---
+++ 01.Count Real Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+            SortedDictionary<double, int> keyCounter = new SortedDictionary<double, int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (!keyCounter.ContainsKey(numbers[i]))
+                {
+                    keyCounter.Add(numbers[i], 0);
+                }
+                keyCounter[numbers[i]]++;
+            }
+            foreach (var item in keyCounter)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
