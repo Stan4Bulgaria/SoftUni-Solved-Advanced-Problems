4c66ba4516fd8dacbaac6188dee5e5f5178b7e14
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:36:12 +0300

    Add files via upload

---
+++ Custom Softuni String/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Custom_Softuni_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoftuniString fastString = new SoftuniString();
+            fastString.Append("Hell");
+            Console.WriteLine(fastString);
+         
+        }
+    }
+}
---
+++ Custom Softuni String/SoftuniString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Custom_Softuni_String
+{
+    class SoftuniString
+    {
+        private int index = 0;
+        private char[] array;
+        public SoftuniString()
+        {
+            array = new char[4];
+        }
+        public void Append(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                array[index++] = text[i];
+            }
+        }
+        public override string ToString()
+        {
+            return new string(array);
+        }
+    }
+}
