1e109128830ec78e94e68ca6adf29ad421d6f7cf
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:20:34 +0300

    Add files via upload

---
+++ Largest 3 Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+            numbers = numbers.OrderByDescending(n => n).Take(3).ToList();
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+        
+    }
+}
