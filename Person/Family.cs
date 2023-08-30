3d720e3058f7c2fce8e6fa3f5b5e3215b6df76ed
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 10:58:23 +0300

    Add files via upload

---
+++ Person/Family.cs
@@ -0,0 +1,35 @@
+﻿
+using DefiningClasses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DifiningClasses
+{
+    class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+        public void AddMember(Person familMembers)
+        {
+            People.Add(familMembers);
+        }
+        public Person GetOldestMember()
+        {
+            int oldest = int.MinValue;
+            Person oldestFam = null;
+            for (int i = 0; i < People.Count; i++)
+            {
+                if (People[i].Age > oldest)
+                {
+                    oldestFam = People[i];
+                    oldest = oldestFam.Age;
+                }
+            }
+            return oldestFam;
+        }
+    }
+}
---
+++ Person/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using DifiningClasses;
+using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+   {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] inpute = Console.ReadLine().Split();
+                string name = inpute[0];
+                int age = int.Parse(inpute[1]);
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+            Person oldestFamily = family.GetOldestMember();
+            Console.WriteLine($"{oldestFamily.Name} {oldestFamily.Age}");
+        }
+   }
+}
