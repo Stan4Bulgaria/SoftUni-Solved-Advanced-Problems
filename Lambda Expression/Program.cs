0ca2b498c52878f10d06d361a07439ac2c986ddb
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:29:19 +0300

    Add files via upload

---
+++ Lambda Expression/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lambda_Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>()
+                { 1,2,3,4,5,6,7,8,9,10};
+            list = list.Where(x => x % 2 == 0).ToList();
+            Console.WriteLine(string.Join(" ", list));
+            List<int> list2 = new List<int>()
+                { 1,2,3,4,5,6,7,8,9,10};
+            list2 = list2.Where(x => x % 2 != 0).ToList();
+            Console.WriteLine(string.Join(" ", list2));
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            array = array.Where(x => x !=0 ).ToArray();
+            Console.WriteLine(string.Join(" ", array));
+
+
+            //List<int> list = new List<int>()
+            //{ 1,2,3,4,5,6,7,8,9,10};
+            //list = list.Where(isEven).ToList();
+            //Console.WriteLine(string.Join(" ", list));
+        }
+        
+    }
+}
