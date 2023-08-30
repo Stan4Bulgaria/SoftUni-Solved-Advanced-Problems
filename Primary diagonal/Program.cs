c6057bb22aaba6fb70988f09907e4a792b9f9643
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 12:11:42 +0300

    Add files via upload

---
+++ Primary diagonal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Primary_diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int sum = 0;
+            for (int row = 0; row < n; row++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                sum += matrix[row, row];
+                 
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
