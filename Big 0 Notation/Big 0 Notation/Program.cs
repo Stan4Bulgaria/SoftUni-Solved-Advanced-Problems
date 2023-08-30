6c0c0b0df8d2bd557da8f1c11b717f6e95c321a1
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:34:02 +0300

    Add files via upload

---
+++ Big 0 Notation/Big 0 Notation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Big_0_Notation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+        bool IsFirstElementNull(IList<String> elements)
+        {
+            return elements[0] == null;
+        }
+    }
+}
