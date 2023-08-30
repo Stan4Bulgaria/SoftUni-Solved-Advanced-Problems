390e4816341fcf4e64c3845b6a7163d1c612d00f
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:05:12 +0300

    Add files via upload

---
+++ Even and Odd Subtraction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+namespace Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumEvenNums = 0;
+            int sumOddNums = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] % 2 == 0)
+                {
+                    sumEvenNums += nums[i];
+                }
+                else
+                {
+                    sumOddNums += nums[i];
+                }
+            }
+            double diff = (double)(sumEvenNums - sumOddNums);
+            Console.WriteLine(diff);
+        }
+    }
+}
