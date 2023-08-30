43d54e05f35e71d78dcb2c51337d967ed0d63085
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:28:11 +0300

    Add files via upload

---
+++ Dictionary Order By/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary_Order_By
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> countryTowns = new Dictionary<string, List<string>>();
+        }
+    }
+}
