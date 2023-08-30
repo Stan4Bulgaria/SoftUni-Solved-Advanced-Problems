484cbdbafae92f595e287abcd5213b90fed75db8
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:47:00 +0300

    Add files via upload

---
+++ Ages/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var age = int.Parse(Console.ReadLine());
+            string typeOfPerson = string.Empty;
+            switch (age)
+            {
+                case int num when num <= 2:
+                    typeOfPerson = "baby";
+                    break;
+                case int num when num > 2 && num<=13:
+                    typeOfPerson = "child";
+                    break;
+                case int num when num >13 && num <=19:
+                    typeOfPerson = "teenager";
+                    break;
+                case int num when num > 19 && num <= 65:
+                    typeOfPerson = "adult";
+                    break;
+                default:
+                    typeOfPerson = "elder";
+                    break;
+
+            }
+            Console.WriteLine(typeOfPerson);
+        }
+    }
+}
