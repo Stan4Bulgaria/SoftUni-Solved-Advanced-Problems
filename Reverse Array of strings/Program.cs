55b8bf09752e04cea5d4b75844559e2944267e22
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:04:47 +0300

    Add files via upload

---
+++ Reverse Array of strings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_Array_of_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] symbols = Console.ReadLine().Split().ToArray();
+            for (int i = symbols.Length - 1; i >=0; i--)
+            {
+                Console.Write($"{symbols[i]} ");
+            }
+        }
+    }
+}
