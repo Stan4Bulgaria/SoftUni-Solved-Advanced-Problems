e0e6dd191793d3845ab6b63036d423d7ffe2a33b
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:50:11 +0300

    Add files via upload

---
+++ Print and Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Print_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string correctPass = "";
+            int incorrectCount = 0;
+            for (int i = username.Length -1 ; i>=0; i--)
+            {
+                correctPass += username[i];
+            }
+            string password = Console.ReadLine();
+            while (password != correctPass)
+            {
+                incorrectCount++;
+                if (incorrectCount == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+                password = Console.ReadLine();
+
+            }
+            if (password == correctPass)
+            {
+                Console.WriteLine($"User {username} logged in.");
+            }
+        }
+    }
+}
