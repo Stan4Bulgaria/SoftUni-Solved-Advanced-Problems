caa172f8d9ed826612c5245f13308cb15e312633
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:05:45 +0300

    Add files via upload

---
+++ Equal Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+namespace Equal_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arrayTwo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumArrayOne = 0;
+            int sumArrayTwo = 0;
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                if(arrayOne[i] != arrayTwo[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+                else
+                {
+                    sumArrayOne += arrayOne[i];
+                    sumArrayTwo += arrayTwo[i];
+                    
+                }
+            }
+            Console.WriteLine($"Arrays are identical. Sum: {sumArrayOne}");
+
+        }
+    }
+}
