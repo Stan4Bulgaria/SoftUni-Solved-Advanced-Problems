81e2395697262268c5c439b8cf6a5ffd94dab2a2
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:53:46 +0300

    Add files via upload

---
+++ Message translater/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Message_translater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Regex ways = new Regex(@"\!([A-Z][a-z]{2,})\!\:\[([a-zA-z]{8,})\]");
+
+            for (int i = 0; i < num; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match match = ways.Match(input);
+
+                if (match.Success)
+                {
+                    string name = match.Groups[2].ToString();
+                    string nums = string.Empty;
+                    for (int j = 0; j < name.Length; j++)
+                    {
+                        int digit = name[j];
+                        nums += digit + " ";
+
+                    }
+                    Console.WriteLine($"{match.Groups[1]}: {nums}");
+                }
+                else
+                {
+                    Console.WriteLine("The message is invalid");
+                }
+            }
+        }
+    }
+}
