5f4d15ce3240bd4c3a21cc30cfbaa5ff6c786450
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:26:23 +0300

    Add files via upload

---
+++ Creating pages/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Creating_pages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Enumerable.Range(0, 100).ToList();
+            int page = int.Parse(Console.ReadLine()) -1 ;
+            int count = 10;
+            list = list.Skip(page * count).Take(count).ToList();
+            Console.WriteLine(string.Join(" ", list));
+
+        }
+    }
+}
