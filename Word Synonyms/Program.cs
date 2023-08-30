319187924e33cc8853377719e1a48b2b97725a17
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:18:20 +0300

    Add files via upload

---
+++ Word Synonyms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> wordSynonyms = new Dictionary<string, List<string>>();
+            for (int i = 0; i <n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (!wordSynonyms.ContainsKey(word))
+                {
+                    wordSynonyms.Add(word, new List<string>());
+                }
+                wordSynonyms[word].Add(synonym);
+            }
+            foreach (var item in wordSynonyms)
+            {
+                Console.WriteLine($"{item.Key} - {String.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
