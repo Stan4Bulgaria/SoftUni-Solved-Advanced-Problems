735b223e2a4c4dbe64b89c37f071f7175628c950
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:45:41 +0300

    Add files via upload

---
+++ A Miner Task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfMetal = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            Dictionary<string, int> metalAndQuantity = new Dictionary<string, int>();
+            while(typeOfMetal != "stop")
+            {
+                if (!metalAndQuantity.ContainsKey(typeOfMetal))
+                {
+                    metalAndQuantity.Add(typeOfMetal, quantity);
+                }
+                else
+                {
+                    metalAndQuantity[typeOfMetal] += quantity;
+                }
+                typeOfMetal = Console.ReadLine();
+                if(typeOfMetal == "stop")
+                {
+                    break;
+                }
+                quantity = int.Parse(Console.ReadLine());
+            }
+            foreach (var item in metalAndQuantity)
+            {
+                Console.Write($"\n{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
