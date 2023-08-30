b79f252d8da8c6d8132fb140633811c11048d80e
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:57:31 +0300

    Add files via upload

---
+++ GenericBoxOfString/Box.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        public Box()
+        {
+            this.Items = new List<T>(); 
+        }
+        public List<T> Items { get; set; }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var item in Items)
+            {
+                stringBuilder.AppendLine($"{typeof(T)}: {item}");
+            }
+            return stringBuilder.ToString();
+
+        }
+        public void Swap(int firstIndex,int secondIndex)
+        {
+            var toBeSwaped = Items[firstIndex];
+            Items[firstIndex] = Items[secondIndex];
+            Items[secondIndex] = toBeSwaped;
+        }
+        public int CountGreaterThan(T itemToCompare)
+        {
+            int counter = 0;
+            foreach (var item in Items )
+            {
+                if (item.CompareTo(itemToCompare) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+    }
+}
---
+++ GenericBoxOfString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<double> box = new Box<double>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine()); 
+
+                box.Items.Add(input);
+
+            }
+            //    string[] swap = Console.ReadLine().Split();
+            //    int firstIndex = int.Parse(swap[0]);
+            //    int secondIndex = int.Parse(swap[1]);   
+            //    box.Swap(firstIndex, secondIndex);
+            //    Console.WriteLine(box.ToString());
+            double inpute =double.Parse(Console.ReadLine());
+            int number = box.CountGreaterThan(inpute);
+            Console.WriteLine(number);
+        }
+    }
+}
