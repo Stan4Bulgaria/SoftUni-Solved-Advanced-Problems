6a657a9b9ebe415a34e011d117005235194376df
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:38:53 +0300

    Add files via upload

---
+++ Count Chars in a String/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] word = Console.ReadLine().Split();
+            Dictionary<char, int> charAndHisOccurrences = new Dictionary<char, int>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                string tempWord = word[i];
+                for (int j = 0; j < tempWord.Length; j++)
+                {
+                    if (!charAndHisOccurrences.ContainsKey(tempWord[j]))
+                    {
+                        charAndHisOccurrences.Add(tempWord[j], 0);
+                    }
+                    charAndHisOccurrences[tempWord[j]]++;
+                }
+            }
+            foreach (var item in charAndHisOccurrences)
+            {
+                Console.Write($"\n{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
