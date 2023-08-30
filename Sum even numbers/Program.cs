7e21c2a7f51a75dbcd7bfe9838353aadcdda0909
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:03:13 +0300

    Add files via upload

---
+++ Sum even numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_even_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumOfEvenNum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] % 2 == 0)
+                {
+                    sumOfEvenNum += nums[i];
+                }
+            }
+            Console.WriteLine(sumOfEvenNum);
+        }
+    }
+}
