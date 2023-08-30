e8dab62b090a4f82ea14cf4bea11c47c24d5c8e2
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 10:55:14 +0300

    Add files via upload

---
+++ 04.Fast food/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Fast_food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityForTheDay = int.Parse((Console.ReadLine()));
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(orders);
+            Console.WriteLine(queue.Max());
+            foreach (var item in queue)
+            {
+                if(item <= quantityForTheDay)
+                {
+                    quantityForTheDay -=  item;
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {item}");
+                    return;
+                }
+            }
+            Console.WriteLine($"Orders complete");
+            
+           
+        }
+    }
+}
