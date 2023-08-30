c91a2c492682d4aea2e50b9a9dc6c25d27f8038c
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:06:20 +0300

    Add files via upload

---
+++ Condense Array to Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+namespace Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int [] condesed = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                 condesed [i] = nums[i] + nums[i + 1];
+                nums[i] = condesed[i];
+            }
+            Console.WriteLine(condesed);
+            
+        }
+    }
+}
