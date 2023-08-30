2928bdd03bb7cabf1aae8d965b31a3125de494b0
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:48:58 +0300

    Add files via upload

---
+++ Padawan Equipment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Padawan_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double amountOfMoney = double.Parse(Console.ReadLine());
+            int countOfStudents = int.Parse(Console.ReadLine());
+            //double lighSaberPrice = double.Parse(Console.ReadLine());
+           // double robePrice = double.Parse(Console.ReadLine());
+            double beltPrice = double.Parse(Console.ReadLine());
+           // lighSaberPrice = lighSaberPrice * (countOfStudents +(Math.Ceiling(countOfStudents * 0.10)));
+            //robePrice = robePrice * countOfStudents;
+            if(countOfStudents % 6 == 0)
+            {
+                 
+                
+            }
+            else
+            {
+                beltPrice = beltPrice * countOfStudents;
+            }
+            
+            
+
+        }
+    }
+}
