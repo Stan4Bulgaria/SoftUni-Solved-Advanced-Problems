92f8453635d3ce777ac0f027de944dea9d0f1f11
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 11:30:25 +0300

    Add files via upload

---
+++ Sorting Dictionaries/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorting_Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<int, string> userAges = new SortedDictionary<int, string>();
+            userAges[1] = "Pesho";
+            userAges[2] = "Mistafa";
+            userAges[3] = "Arsen";
+            userAges[4] = "Gavril";
+            userAges[5] = "Dobi";
+            foreach (var item in userAges)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
