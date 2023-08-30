d9aed1bdb1f08741325b096c370ad509dfbb1286
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 10:55:51 +0300

    Add files via upload

---
+++ 05. Fashion Boutique/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inpute = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rackCapacity = int.Parse(Console.ReadLine());
+        }
+    }
+}
